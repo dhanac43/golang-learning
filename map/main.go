7983ad3f9800094607b7a5b6846ab077e5b015c5
dhanac43 <dhana.lakshmi@river-island.com>
2026-03-16 09:40:13 +0000

    map training

--- map/main.go
+++ map/main.go
@@ -69,4 +69,36 @@ func main() {
 	for word, count := range wordCount {
 		fmt.Printf(" %s : %d\n", word, count)
 	}
+
+	//Exercise:3:-Use the switch-without-expression form to convert a numeric score into a letter grade.
+
+	score := 73
+
+	switch {
+	case score >= 90:
+		fmt.Println("Grade: A")
+	case score >= 80:
+		fmt.Println("Grade: B")
+	case score >= 70:
+		fmt.Println("Grade: C")
+	case score >= 60:
+		fmt.Println("Grade: D")
+	default:
+		fmt.Println("Grade: F")
+	}
+
+	//Exercise 4: Event Handler with fallthrough
+
+	event := "EventCancelled"
+
+	switch event {
+	case "EventCancelled":
+		fallthrough
+	case "EventReturnRequested":
+		fmt.Println("Refund initiated")
+	case "EventDispatched":
+		fmt.Println("Order shipped")
+	default:
+		fmt.Println("Unhandled event")
+	}
 }
